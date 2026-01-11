7ab58ed5ea12b61ef3bf0c98c87938df013ce4ab
Sayaji Jadhav <sayaji.v.jadhav@gmail.com>
2026-01-11 10:08:11 +0530

    changes in demo java file

--- TestBankApplication/src/com/soft/Demo.java
+++ TestBankApplication/src/com/soft/Demo.java
@@ -4,6 +4,7 @@ package com.soft;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("welcome to java programming");
+		System.out.println("welcome to github....");
 	}
 
 }
